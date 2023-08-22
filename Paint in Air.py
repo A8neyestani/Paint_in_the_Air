a294ccd1328645a0184caf90603b07f7512c0e29
Arman neyestani <82177416+A8neyestani@users.noreply.github.com>
2023-08-22 12:21:57 +0200

    Update Paint in Air.py

--- Paint in Air.py
+++ Paint in Air.py
@@ -1,85 +1,99 @@
+"""
+Interactive Drawing with Hand Detection
+Author: Arman Neyestani 
+https://github.com/A8neyestani
+A8neyestani@protonmail.com
+
+
+This module captures video from the webcam and allows users to draw lines by tracking their hand movements.
+It also offers palette selections to change the color or use an eraser.
+"""
+
 import cv2
-import time
-import os
 import handTrackingModule as htm
-import numpy as np
 
-wCam, hCam = 640, 480
-eraser=cv2.imread('eraser.png')
-eraser=cv2.resize(eraser,(100,100))
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
+def initialize_capture(width, height):
+    """Initialize video capture with specified width and height."""
+    cap = cv2.VideoCapture(0)
+    cap.set(3, width)
+    cap.set(4, height)
+    return cap
 
-CTime = 0
-PTime = 0
-color=(0,0,255)
-
-
-detector = htm.handDetector()
-EraserBox={'x1':500,'y1':0,'x2':600,'y2':100}
-redplt={'x1':100,'y1':0,'x2':200,'y2':100}
-blueplt={'x1':300,'y1':0,'x2':400,'y2':100}
-
-
-fingerX=[]
-while True:
-    _, frame = cap.read()
-    if frame is None:
-        break
+def draw_palettes(frame, eraser_image, boxes):
+    """Draw eraser and color palettes on the frame."""
+    frame[boxes['eraser']['y1']:boxes['eraser']['y2'], boxes['eraser']['x1']:boxes['eraser']['x2']] = eraser_image
     
-    img = detector.findHands(frame)
-    frame=cv2.rectangle(frame,(EraserBox['x1'],EraserBox['y1']),
-                              (EraserBox['x2'],EraserBox['y2']),
-                              (0,0,255),3)
-    frame=cv2.rectangle(frame,(redplt['x1'],redplt['y1']),
-                              (redplt['x2'],redplt['y2']),
-                              (0,0,255),-1)
-    frame=cv2.rectangle(frame,(blueplt['x1'],blueplt['y1']),
-                              (blueplt['x2'],blueplt['y2']),
-                              (255,0,0),-1)
+    for key, box in boxes.items():
+        if key != 'eraser':
+            color = box['color']
+            thickness = -1 if 'plt' in key else 3
+            frame = cv2.rectangle(frame, (box['x1'], box['y1']), (box['x2'], box['y2']), color, thickness)
 
-    frame[EraserBox['y1']:EraserBox['y2'],EraserBox['x1']:EraserBox['x2']]=eraser
+    return frame
 
-    lmList = detector.findPosition(img, draw= False)
-    j=1
-    
-    
+def handle_interactions(lmList, boxes):
+    """Manage interactions with the Eraser and color palettes."""
+    color = None
+    clear = False
 
-    if len(lmList)> 10:
+    try:
+        if boxes['eraser']['x1'] < lmList[8][1] < boxes['eraser']['x2'] and boxes['eraser']['y1'] < lmList[8][2] < boxes['eraser']['y2']:
+            clear = True
+        for key, box in boxes.items():
+            if 'plt' in key and box['x1'] < lmList[8][1] < box['x2'] and box['y1'] < lmList[8][2] < box['y2']:
+                color = box['color']
+    except IndexError:
+        pass
 
-        if abs(lmList[8][1]-lmList[5][1])>25:
-            fingerX.append([lmList[8][1],lmList[8][2]])
+    return color, clear
 
-    for i in range(1,len(fingerX)-1):
-        
-            
-        frame= cv2.line(frame,(fingerX[j-1][0],fingerX[j-1][1]),
-        (fingerX[j][0],fingerX[j][1])
-        ,color,5)
+def main():
+    WIDTH, HEIGHT = 640, 480
+    eraser_image = cv2.resize(cv2.imread('eraser.png'), (100, 100))
+    detector = htm.handDetector()
 
-    #cv2.line(img,(0,0),(511,511),(255,0,0),5)
+    boxes = {
+        'eraser': {'x1': 500, 'y1': 0, 'x2': 600, 'y2': 100},
+        'redplt': {'x1': 100, 'y1': 0, 'x2': 200, 'y2': 100, 'color': (0, 0, 255)},
+        'blueplt': {'x1': 300, 'y1': 0, 'x2': 400, 'y2': 100, 'color': (255, 0, 0)}
+    }
 
-        j+=1
+    finger_positions = []
+    current_color = (0, 0, 255)  # Default color: red
 
+    cap = initialize_capture(WIDTH, HEIGHT)
 
-       # print(fingerX)
-    try:   
-        if EraserBox['x1']<lmList[8][1]<EraserBox['x2'] and EraserBox['y1']<lmList[8][2]<EraserBox['y2'] :
-            fingerX=[]
-        if redplt['x1']<lmList[8][1]<redplt['x2'] and redplt['y1']<lmList[8][2]<redplt['y2'] :
-            color=(0,0,255)
-        if blueplt['x1']<lmList[8][1]<blueplt['x2'] and blueplt['y1']<lmList[8][2]<blueplt['y2'] :
-            color=(255,0,0)
-    except :
-        None
-    
-# cv2.add(frame,eraser)
-    cv2.imshow("image", frame)
-    #cv2.imshow('de',eraser)
+    while True:
+        _, frame = cap.read()
+        if frame is None:
+            break
 
-    if cv2.waitKey(1) == ord("q"):
-        break
-#cv2.imshow('de',eraser)
-cv2.destroyAllWindows()
-cap.release()
+        frame = detector.findHands(frame)
+        frame = draw_palettes(frame, eraser_image, boxes)
+
+        lmList = detector.findPosition(frame, draw=False)
+
+        if len(lmList) > 10 and abs(lmList[8][1] - lmList[5][1]) > 25:
+            finger_positions.append([lmList[8][1], lmList[8][2]])
+
+        for i in range(1, len(finger_positions)):
+            frame = cv2.line(frame, tuple(finger_positions[i-1]), tuple(finger_positions[i]), current_color, 5)
+
+        new_color, erase = handle_interactions(lmList, boxes)
+
+        if new_color:
+            current_color = new_color
+
+        if erase:
+            finger_positions.clear()
+
+        cv2.imshow("Interactive Drawing", frame)
+
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+if __name__ == "__main__":
+    main()
